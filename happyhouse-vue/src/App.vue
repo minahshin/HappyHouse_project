f23beff47295907ae3f553062e94e9fc816856a2
양소정 <thwjd793@naver.com>
2022-05-18 20:21:38 +0900

    header적용

--- happyhouse-vue/src/App.vue
+++ happyhouse-vue/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <navi-bar />
     <router-view />
-  </nav>
+  </div>
 </template>
 
+<script>
+import NaviBar from "@/components/layout/HeaderNaviBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NaviBar,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+a {
+  text-decoration: none;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+a:hover {
+  text-decoration: none;
   font-weight: bold;
-  color: #2c3e50;
+  color: rgb(0, 253, 147);
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+a.router-link-exact-active {
+  color: #ffffff;
 }
 </style>
---
+++ happyhouse-vue/src/components/layout/HeaderNaviBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">
+        <router-link to="/">
+          <img
+            src="@/assets/류피.png"
+            class="d-inline-block align-middle"
+            width="80px"
+            alt="ssafy"
+          />
+        </router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'home' }" class="link"
+              ><b-icon icon="house" font-scale="1.5"></b-icon> Home
+            </router-link></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link">
+              시세검색</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link">
+              즐겨찾기</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link">
+              지역정보</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link">
+              상권검색</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#" class="qna"
+            ><router-link :to="{ name: 'questionView' }" class="link">
+              QNA</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link">
+              About Us</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: '' }" class="link"
+              >Site Map</router-link
+            ></b-nav-item
+          >
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-icon icon="people" font-scale="2"></b-icon>
+            </template>
+            <b-dropdown-item href="#"
+              ><router-link :to="{ name: 'signUp' }" class="link"
+                ><b-icon icon="person-circle"></b-icon> 회원가입</router-link
+              ></b-dropdown-item
+            >
+            <b-dropdown-item href="#"
+              ><router-link :to="{ name: 'signIn' }" class="link"
+                ><b-icon icon="key"></b-icon> 로그인</router-link
+              ></b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNaviBar",
+};
+</script>
+
+<style></style>
