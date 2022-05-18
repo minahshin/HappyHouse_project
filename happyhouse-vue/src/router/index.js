efa7d2e73a251034ef27643a3bf4e82a86a50dd9
minah741 <minah741@naver.com>
2022-05-18 16:42:51 +0900

    router 설정

--- happyhouse-vue/src/router/index.js
+++ happyhouse-vue/src/router/index.js
@@ -11,13 +11,26 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/question",
+    name: "question",
+    component: () => import("@/views/QuestionListView.vue"),
+    children: [
+      {
+        path: ":qno",
+        name: "questionView",
+        component: () => import("@/components/qna/QuestionView.vue"),
+      },
+      {
+        path: "update",
+        name: "questionUpdate",
+        component: () => import("@/components/qna/QuestionUpdateView.vue"),
+      },
+      {
+        path: "regist",
+        name: "questionRegist",
+        component: () => import("@/components/qna/QuestionRegistView.vue"),
+      },
+    ],
   },
 ];
 
