659a1446c5d0eb25fe5a0dc4a11180f8ff3efc0a
양소정 <thwjd793@naver.com>
2022-05-19 19:12:49 +0900

    공지사항은 날짜만 undefined/Memver 차후 수정 예정

---
+++ happyhouse-vue/src/components/notice/NoticeListView.vue
@@ -0,0 +1,76 @@
+<template>
+  <b-container class="bv-example-row mt-3">
+    <b-row>
+      <b-col>
+        <b-alert show><h3>글목록</h3></b-alert>
+      </b-col>
+    </b-row>
+    <b-row class="mb-1">
+      <b-col class="text-right">
+        <b-button variant="outline-primary" @click="moveWrite()"
+          >글쓰기</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col v-if="articles.length">
+        <b-table-simple hover responsive>
+          <b-thead head-variant="dark">
+            <b-tr>
+              <b-th>글번호</b-th>
+              <b-th>카테고리</b-th>
+              <b-th>제목</b-th>
+              <b-th>작성자</b-th>
+              <b-th>작성일</b-th>
+            </b-tr>
+          </b-thead>
+          <tbody>
+            <notice-list-item
+              v-for="article in articles"
+              :key="article.nno"
+              v-bind="article"
+            />
+          </tbody>
+        </b-table-simple>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import http from "@/api/http";
+import NoticeListItem from "@/components/notice/item/NoticeListItem";
+
+export default {
+  name: "NoticeList",
+  components: {
+    NoticeListItem,
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  created() {
+    http.get(`/notice`).then(({ data }) => {
+      this.articles = data;
+    });
+  },
+  methods: {
+    moveWrite() {
+      this.$router.push({ name: "noticeRegist" });
+    },
+  },
+};
+</script>
+
+<style scope>
+.tdClass {
+  width: 50px;
+  text-align: center;
+}
+.tdSubject {
+  width: 300px;
+  text-align: left;
+}
+</style>
---
+++ happyhouse-vue/src/components/notice/item/NoticeListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-tr>
+    <b-td>{{ nno }}</b-td>
+    <b-td>{{ category }}</b-td>
+    <b-th class="text-left">
+      <router-link :to="{ name: 'noticeView', params: { nno: nno } }">{{
+        subject
+      }}</router-link>
+    </b-th>
+    <b-td>{{ writer }}</b-td>
+    <b-td>{{ regDate | dateFormat }}</b-td>
+  </b-tr>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "NoticeListItem",
+  props: {
+    nno: Number,
+    writer: String,
+    category: String,
+    subject: String,
+    regDate: String,
+  },
+  filters: {
+    dateFormat(regDate) {
+      return moment(new Date(regDate)).format("YY.MM.DD");
+    },
+  },
+};
+</script>
+
+<style></style>
