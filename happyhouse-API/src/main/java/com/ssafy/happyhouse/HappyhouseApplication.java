da7f17ce0de7d96faad6cd48bba8ca0e8b435d78
minah741 <minah741@naver.com>
2022-05-20 11:53:24 +0900

    Cors 설정 및 QNA Update

--- happyhouse-API/src/main/java/com/ssafy/happyhouse/HappyhouseApplication.java
+++ happyhouse-API/src/main/java/com/ssafy/happyhouse/HappyhouseApplication.java
@@ -18,7 +18,7 @@ public class HappyhouseApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
