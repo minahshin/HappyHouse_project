cd3ed9bc206b24bc9a3bc3c16c854f5f4186fa75
양소정 <thwjd793@naver.com>
2022-05-25 09:03:39 +0900

    즐겨찾기 백엔드

---
+++ happyhouse-API/src/main/java/com/ssafy/happyhouse/model/service/FavoriteServiceDao.java
@@ -0,0 +1,45 @@
+package com.ssafy.happyhouse.model.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.happyhouse.model.FavoriteDto;
+import com.ssafy.happyhouse.model.mapper.FavoriteMapper;
+
+@Repository
+public class FavoriteServiceDao implements FavoriteService{
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Autowired
+	private FavoriteMapper favoriteMapper;
+
+	@Override
+	public List<FavoriteDto> showFavoriteList(String memberId) throws Exception {
+		
+		return favoriteMapper.showFavoriteList(memberId);
+	}
+
+	@Override
+	public void registerFavorite(FavoriteDto favoriteDto) throws Exception {
+		
+		favoriteMapper.registerFavorite(favoriteDto);
+	}
+
+	@Override
+	public void deleteFavorite(FavoriteDto favoriteDto) throws Exception {
+		
+		favoriteMapper.deleteFavorite(favoriteDto);
+	}
+
+	@Override
+	public FavoriteDto viewFavorite(String aptName) throws Exception {
+		
+		return favoriteMapper.viewFavorite(aptName);
+	}
+
+}
