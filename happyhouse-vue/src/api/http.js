d5cf3241cf71c1033ac7f8c7ebcb5ed76509e10a
minah741 <minah741@naver.com>
2022-05-27 04:23:53 +0900

    배포 준비 완료

--- happyhouse-vue/src/api/http.js
+++ happyhouse-vue/src/api/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://localhost",
+  baseURL: "http://ec2-3-39-24-149.ap-northeast-2.compute.amazonaws.com:9999",
   headers: {
     "Content-type": "application/json",
   },
